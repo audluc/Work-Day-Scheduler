2a85fa0e48630fe809a7234c130fecd31478257f
Audra Dagenais <66926112+audluc@users.noreply.github.com>
2020-08-02 16:10:00 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     console.log("ready!");
     let current = moment().hour();
     let todos = []
+
+    //color change depending on time
     $(".time-block").each(function (index) {
         let id = parseInt($(this).attr("id"))
         console.log(id, current);
@@ -15,7 +17,16 @@ $(document).ready(function () {
         else {
             $(this).addClass("future")
         }
+        // local storage 
+        let dataStored = localStorage.getItem(id);
+        console.log('dataStored: ${dataStored}');
+        
+        //textarea for calendar
+        console.log('this: ${this}');
+        console.log('this > "textarea": ${JSON.stringify($(this > "textarea))}')
+        $(this).find("texarea").val(dataStored);
     });
+
     $(".saveBtn").click(function () {
         console.log("click")
         let value = $(this).siblings(".description").val()
@@ -23,13 +34,20 @@ $(document).ready(function () {
         todos.push({ value: value, time: time })
         console.log(todos)
 
-    })
-});
+        //current date
+        let timeblock = moment().format("MMM Do YY");
+        $(".time-block").append(timeblock);
 
 
 
+        var descriptionInput = document.querySelector("#description");
+        var saveBtn = description.querySelector("description")
 
-
-
-
-
+        function renderLastRegistered() {
+            var description = localStorage.getItem(".description");
+            if (!description) {
+                return;
+            }
+        }
+    });
+})
