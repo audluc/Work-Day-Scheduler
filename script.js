218fd88f19f05c25768a4179e384f43a7d300c1e
Audra Dagenais <66926112+audluc@users.noreply.github.com>
2020-08-04 18:25:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-// A $( document ).ready() block.
+//current date//////
+let timeblock = moment().format("MMM Do YYYY");
+$(".time-block").append(timeblock);
+console.log('current day')
+
+// local storage////
 $(document).ready(function () {
     console.log("ready!");
     let current = moment().hour();
-    let todos = []
+    let todos = JSON.parse(localStorage.getItem("todos")) || []
+    console.log(todos)
 
-    //color change depending on time
+    //color change depending on time//////
     $(".time-block").each(function (index) {
         let id = parseInt($(this).attr("id"))
         console.log(id, current);
@@ -17,13 +23,13 @@ $(document).ready(function () {
         else {
             $(this).addClass("future")
         }
-        // local storage 
+        // local storage /////
         let dataStored = localStorage.getItem(id);
         console.log('dataStored: ${dataStored}');
-        
-        //textarea for calendar
+
+        //textarea for calendar////
         console.log('this: ${this}');
-        console.log('this > "textarea": ${JSON.stringify($(this > "textarea))}')
+        console.log('this > "textarea": ${JSON.stringify($(this > "textarea"))}')
         $(this).find("texarea").val(dataStored);
     });
 
@@ -33,21 +39,21 @@ $(document).ready(function () {
         let time = $(this).parent().attr("id")
         todos.push({ value: value, time: time })
         console.log(todos)
-
-        //current date
-        let timeblock = moment().format("MMM Do YY");
-        $(".time-block").append(timeblock);
-
-
+        localStorage.setItem("todos", JSON.stringify(todos))
 
         var descriptionInput = document.querySelector("#description");
-        var saveBtn = description.querySelector("description")
+        var saveBtn = document.querySelector("description")
 
-        function renderLastRegistered() {
-            var description = localStorage.getItem(".description");
-            if (!description) {
-                return;
-            }
-        }
+        
     });
+    function renderLastRegistered() {
+        var description = localStorage.getItem("todos");
+        console.log(description)
+        if (!description) {
+            return;
+        }
+
+
+    }
+    renderLastRegistered()
 })
